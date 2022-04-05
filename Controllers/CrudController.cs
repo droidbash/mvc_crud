19f74b520ac014efa63514c3bf18619fb51ae46d
Diego <diego@islevalu.net>
2022-04-05 14:35:27 -0700

    reupload test

---
+++ Controllers/CrudController.cs
@@ -0,0 +1,184 @@
+﻿/*
+ * =====================================================================================================================================
+ * USINGS
+ */
+using System;
+using mvc_crud.Models;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Web.Mvc;
+using System.Web.Helpers;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Web;
+/*
+* =====================================================================================================================================
+* NOT USED
+*/
+//using System.Linq;
+//using System.Web;
+namespace mvc_crud.Controllers
+{
+    public class CrudController : Controller
+    {
+        /*
+         * =====================================================================================================================================
+         * ACTION RESULTS
+         */
+        public ActionResult Index()
+        {
+            return View(GetDrivers());
+        }
+        /*
+         * =====================================================================================================================================
+         * METHODS
+         */
+        [HttpPost]
+        public JsonResult DriverSelect(int id)
+        {
+            foreach(DriverModel driver in GetDrivers())
+            {
+                if(driver.Id == id)
+                    return Json(JsonConvert.SerializeObject(driver));
+            }
+            return null;
+        }
+        [HttpPost]
+        public async Task<ActionResult> DriverModal()
+        {
+            var function = Request.Form["function"];
+            Debug.WriteLine(function);
+            string query;
+            switch (function)
+            {
+                case "edit":
+                    query = QueryEditDriver(Request);
+                    break;
+                case "add":
+                    query = QueryAddDriver(Request);
+                    break;
+                default:
+                    return View("NotImplemented");
+            }
+            try
+            {
+                await QueryAsync(SettingsDevelop, query);
+                return View("Index", await GetDriversAsync());
+            }
+            catch (Exception e)
+            {
+                return View("QueryError", new QueryError() { Msg = "Error al procesar la solicitud en la base de datos.", Error = e.Message });
+            }
+        }
+        /*
+        * DATABASE QUERYS
+        */
+        private string QueryAddDriver(HttpRequestBase request)
+        {
+            var name = request.Form["name"];
+            var nationality = request.Form["nationality"];
+            var age = request.Form["age"];
+            var active = request.Form["active"] == "on";
+            return string.Format("insert into CRUDMVC.dbo.driver (name,nationality,age,active) values ('{0}','{1}','{2}','{3}')", name, nationality, age, active); ;
+            /*
+            return "";
+            */
+        }
+        public string QueryEditDriver(HttpRequestBase request)
+        {
+            var id = request.Form["id"];
+            var name = request.Form["name"];
+            var nationality = request.Form["nationality"];
+            var age = request.Form["age"];
+            var active = request.Form["active"] == "on";
+            return string.Format("update CRUDMVC.dbo.driver set name = '{0}', nationality = '{1}', age = '{2}', active = '{3}' where id = '{4}'", name, nationality, age, active, id);
+        }
+        public string QueryDeleteDriver(int id)
+        {
+            return string.Format("delete from CRUDMVC.dbo.driver where id = '{0}'", id);
+        }
+        public async Task<ActionResult> DeleteDriver(int id)
+        {
+            Debug.WriteLine(id);
+            await QueryAsync(SettingsDevelop, QueryDeleteDriver(id));
+            return View("Index", await GetDriversAsync());
+        }
+        public async Task QueryAsync(ConnectionStringSettings settings, string query)
+        {
+            using (SqlConnection connection = new SqlConnection(settings.ConnectionString))
+            {
+                await connection.OpenAsync();
+                SqlCommand command = connection.CreateCommand();
+                command.CommandText = query;
+                await command.ExecuteNonQueryAsync();
+            }
+        }
+        /*
+         * =====================================================================================================================================
+         * DATABASE GETTERS
+         */
+        string driversQuery = "SELECT id, name, nationality, age, active FROM CRUDMVC.dbo.Driver";
+        ConnectionStringSettings SettingsDevelop = ConfigurationManager.ConnectionStrings["develop"];
+        public List<DriverModel> GetDrivers()
+        {
+            List<DriverModel> drivers = new List<DriverModel>() { };
+            string connectionString = ConfigurationManager.ConnectionStrings["develop"].ConnectionString;
+            
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var command = new SqlCommand(driversQuery, connection);
+                connection.Open();
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        drivers.Add(new DriverModel
+                        {
+                            Id = (int)reader["id"],
+                            Name = (string)reader["name"],
+                            Nationality = (string)reader["nationality"],
+                            Age = (int)reader["age"],
+                            Active = (bool)reader["active"],
+                        });
+                    }
+                }
+            }
+            return drivers;
+        }
+        public async Task<List<DriverModel>> GetDriversAsync()
+        {
+            List<DriverModel> drivers = new List<DriverModel>() { };
+            using (SqlConnection connection = new SqlConnection(SettingsDevelop.ConnectionString))
+            {
+                await connection.OpenAsync();
+                SqlCommand command = connection.CreateCommand();
+                command.CommandText = driversQuery;
+                //await command.ExecuteNonQueryAsync();
+                using (var reader = await command.ExecuteReaderAsync()) {
+                    while (reader.Read())
+                    {
+                        drivers.Add(new DriverModel
+                        {
+                            Id = (int)reader["id"],
+                            Name = (string)reader["name"],
+                            Nationality = (string)reader["nationality"],
+                            Age = (int)reader["age"],
+                            Active = (bool)reader["active"],
+                        });
+                    }
+                }
+            }
+            return drivers;
+        }
+        /*
+        public ActionResult TestView() {
+            List<DriverModel> m = new List<DriverModel>() {
+                new DriverModel { Name = "NAME", Nationality = "NATIONALITY", Age = 20, Active = true }
+            };
+            return View(m);
+        }
+        */
+    }
+}
---
+++ README.md
@@ -0,0 +1,77 @@
+# mvc_crud
+
+﻿El actual ejemplo ha sido elaborado en base a la siguiente referencia:
+https://www.youtube.com/watch?v=z6qZPKBgcaE
+
+Pass data from controller to view
+https://www.c-sharpcorner.com/UploadFile/abhikumarvatsa/various-ways-to-pass-data-from-controller-to-view-in-mvc/
+
+Displaying Data On View From Controller
+https://www.c-sharpcorner.com/UploadFile/3d39b4/displaying-data-on-view-from-controller/
+ViewData Object
+ViewData Model
+ViewBag Object
+TempData Object
+Strongly Typed Views
+
+Tutorials Teacher:
+ViewData
+https://www.tutorialsteacher.com/mvc/viewdata-in-asp.net-mvc
+Model
+https://www.tutorialsteacher.com/mvc/mvc-model#:~:text=Adding%20a%20Model%20Class&text=In%20the%20MVC%20application%20in,name%20Student%20and%20click%20Add.&text=This%20will%20add%20a%20new%20Student%20class%20in%20model%20folder.
+
+Action results
+https://www.variablenotfound.com/2008/05/retornando-actionresults-en-acciones.html
+
+IIS Express Config:
+https://stackoverflow.com/questions/36373963/how-to-leave-iisexpress-running-after-stopping-debugging-in-visual-studio-2015-u
+
+Más ejemplos:
+https://asp.mvc-tutorial.com/views/passing-data-into-views/
+https://stackoverflow.com/questions/25535484/read-data-from-model-and-display-on-razor-view-mvc
+
+====================================================================================
+Bootstrap 3.4:
+https://getbootstrap.com/docs/3.4/
+
+Bootstrap 4.2:
+https://getbootstrap.com/docs/4.2/getting-started/introduction/
+
+href Action of Controller:
+https://stackoverflow.com/questions/596444/html-actionlink-as-a-button-or-an-image-not-a-link
+
+link HTML5 form action to Controller ActionResult
+https://stackoverflow.com/questions/15190929/how-to-link-html5-form-action-to-controller-actionresult-method-in-asp-net-mvc-4
+
+Database with ConnectionStrings of Web.config
+https://docs.microsoft.com/en-us/dotnet/api/system.configuration.configurationmanager.connectionstrings?view=dotnet-plat-ext-6.0
+
+C# ASP.NET MVC: dynamic view update
+https://stackoverflow.com/questions/68639708/c-sharp-asp-net-mvc-update-view-dynamically
+
+TextChanged event in ASP.NET MVC
+https://www.youtube.com/watch?v=gwWf_5hYiDA
+
+Render @section scripts {}
+http://csharprules.blogspot.com/2014/11/usando-el-rendersection-en-mvc.html
+
+Ajax.Form:
+https://www.hanselman.com/blog/aspnet-mvc-preview-4-using-ajax-and-ajaxform
+
+Ajax.BeginForm Form:
+https://www.aspsnippets.com/Articles/ASPNet-MVC-AjaxBeginForm-Submit-Form-without-Page-Refresh-in-ASPNet-MVC.aspx
+
+HTML Table Search:
+https://www.w3schools.com/howto/howto_js_filter_table.asp
+
+C# object to JsonResult:
+https://stackoverflow.com/questions/6201529/how-do-i-turn-a-c-sharp-object-into-a-json-string-in-net
+
+Parse JsonResult in JavaScript:
+https://api.jquery.com/jquery.parsejson/
+
+Async tasks:
+https://docs.microsoft.com/en-us/aspnet/mvc/overview/performance/using-asynchronous-methods-in-aspnet-mvc-4
+
+Expresiones Regulares con acentos:
+https://stackoverflow.com/questions/20690499/concrete-javascript-regular-expression-for-accented-characters-diacritics
