5a3f5bb750c870a16a90cfee5d2ae36643559fc9
droidbash <al16760562@ite.edu.mx>
2022-05-12 14:29:40 -0700

    plugins workaround

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -8,24 +8,25 @@ namespace mvc_crud
         // Para obtener más información sobre las uniones, visite https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
-                        //"~/Scripts/jquery-3.6.0.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
-
-            // Utilice la versión de desarrollo de Modernizr para desarrollar y obtener información. De este modo, estará
-            // para la producción, use la herramienta de compilación disponible en https://modernizr.com para seleccionar solo las pruebas que necesite.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
-
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+            //========================================================================================================================================================
+            //                             ~/VirtualPath                    ~/FileLocations                                                                         //
+            //                                   |                                  |                                                                               //
+            //                                  _|_                                _|_                                                                              //
+            //                                  \ /                                \ /                                                                              //
+            //                                   "                                  "                                                                               //
+                bundles.Add(new ScriptBundle("~/VirtualPath").Include(  "~/FileLocation/FileName1.Extension",                                                       //
+                                                                        "~/FileLocation/FileName2.Extension",                                                       //
+                                                                        "~/FileLocation/FileNameN.Extension"));                                                     //
+            //Estos bundles se pueden llamar desde cualquier parte de la vista con "@Scripts.Render("~/VirtualPath")" que llamará a los archivos de ~/FileLocation. //
+            //De la misma forma se pueden declarar estilos por medio de StyleBundle() y llamarlos con Styles.Render().                                              //
+            //========================================================================================================================================================
+            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/bootstrap.css","~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/signin").Include("~/Content/signin.css"));
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include("~/Scripts/jquery-3.6.0.js"));
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include("~/Scripts/jquery.validate*")); //https://blog.trescomatres.com/2020/06/jquery-validation-validar-segun-una-regla-personalizada/
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include("~/Scripts/modernizr-*"));
+            //bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/bootstrap.js"));
+            bundles.Add(new Bundle("~/bundles/bootstrap").Include("~/Scripts/bootstrap.js"));
         }
     }
 }
