81c7d5e5611183dee2d59cd1361252d6846ceb09
droidbash <al16760562@ite.edu.mx>
2022-06-04 16:53:36 -0700

    DB Connection workaround

---
+++ App_Start/Const.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace mvc_crud
+{
+    public class Const
+    {
+        public enum Tables
+        {
+            Drivers,
+            Cars
+        }
+    }
+}
---
+++ Controllers/EditController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Diagnostics;
+using WebMatrix.Data;
+
+namespace mvc_crud.Controllers
+{
+    public class EditController : Controller
+    {
+        // GET: Edit
+        public ActionResult Table(String @table)
+        {
+            /*
+            String respS = "";
+            foreach(var row in resp)
+            {
+                respS += row.Id;
+            }
+            Debug.WriteLine(respS);
+            */
+            /*
+            switch (table)
+            {
+                case "Cars": return Cars();
+                default: return View();
+            }
+            Debug.WriteLine("ActionResult:" + @table);
+            return RedirectToAction(controllerName:"Edit",actionName:"Cars");
+            */
+            return View();
+        }
+        public ActionResult Cars()
+        {
+            try
+            {
+                var dictionary = new Dictionary<string, object>();
+                Database db = Database.Open("develop");
+                dictionary.Add("data", db.Query("select * from GCAV.dbo.Cars"));
+                //return Json(new { data = "[1,2]"});
+                Debug.WriteLine(Json(new { data = "[1,2]"}).ToString());
+                return null;
+                //return System.Web.Helpers.Json.Encode(dictionary);
+            }
+            catch(Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
